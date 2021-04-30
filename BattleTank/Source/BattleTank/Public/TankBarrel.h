cb3a33b52fe2e4a53307bbbb1efd5c868e7cedbe
abolfazlplp <37250164+abolfazlplp@users.noreply.github.com>
2021-05-01 01:26:39 +0430

    Add files via upload

---
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent)) //, hidecategories = ("Collision"))
+class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	void Elevate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MaxDegreesPerSecond = 10;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MaxElevationDegrees = 40;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float MinElevationDegrees = 0;
+};
---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" 
+class UTankTankAimingComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+	void FoundAimingComponent(UTankAimingComponent* AimCompRef);
+
+	UFUNCTION()
+	void OnPossedTankDeath();
+
+private:
+	void SetPawn(APawn * InPawn);
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick( float DeltaTime ) override;
+
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditDefaultsOnly)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditDefaultsOnly)
+	float CrosshairYLocation = 0.3333;
+
+	UPROPERTY(EditDefaultsOnly)
+	float LineTraceRange = 1000000;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+};
